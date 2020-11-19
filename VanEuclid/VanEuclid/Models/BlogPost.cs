101043578c88d19efce3a9dcfb9c52ef1298789f
Van Euclid <68357572+VanEuclid@users.noreply.github.com>
2020-11-18 20:54:31 -0700

    BlogPost model made

---
+++ VanEuclid/VanEuclid/Models/BlogPost.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace VanEuclid.Models
+{
+    public class BlogPost
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Body { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
