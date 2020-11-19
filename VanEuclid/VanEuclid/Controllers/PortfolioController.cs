25fec64d64534ab2d1ef3459f71e5e3d0732e8a2
Van Euclid <68357572+VanEuclid@users.noreply.github.com>
2020-11-18 17:46:43 -0700

    Incremenatal Add of Portfolio

---
+++ VanEuclid/VanEuclid/Controllers/PortfolioController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace VanEuclid.Controllers
+{
+    public class PortfolioController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View ();
+        }
+
+        public ActionResult Sudoku()
+        {
+            return View();
+        }
+    }
+}
